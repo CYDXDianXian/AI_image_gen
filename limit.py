c6cdda9bcf110c57eed81c666181bd34ad33b695
穿越电线 <lx839071800@163.com>
2022-10-13 11:41:55 +0800

    优化过滤词模块

--- limit.py
+++ limit.py
@@ -7,9 +7,12 @@ wordlist = get_config('ban_word', 'wordlist')
 def guolv(sent=str):
     sent_cp = unicodedata.normalize('NFKC', sent) # 中文标点转为英文
     sent_cp = sent_cp.lower() #转为小写
-    sent_list = sent_cp.split(",") # 从逗号处分开，返回列表
-    print(sent_list)
+    sent_list_ = sent_cp.split(",") # 从逗号处分开，返回列表
     
+    sent_list = []
+    for m in sent_list_:
+        sent_list.append(m.strip()) # 移除空格
+
     # 生成过滤词列表
     tags_guolu_list = []
     for i in sent_list:
